b2209c88d8fc2d99d27bcb22e5e8d405b18554eb
Todd White <todd.white@logos.com>
2019-11-05 13:21:41 -0800

    Remove a bunch of menu items.

--- src/menu/menu_template.js
+++ src/menu/menu_template.js
@@ -1,5 +1,5 @@
 export var getTemplate = app => {
-	var template = [
+	return [
 		{
 			label: 'Edit',
 			submenu: [
@@ -36,119 +36,9 @@ export var getTemplate = app => {
 			label: 'View',
 			submenu: [
 				{
-					role: 'reload',
-				},
-				{
-					type: 'separator',
-				},
-				{
-					role: 'resetzoom',
-				},
-				{
-					role: 'zoomin',
-				},
-				{
-					role: 'zoomout',
-				},
-				{
-					type: 'separator',
-				},
-				{
-					role: 'togglefullscreen',
-				},
-				{
 					role: 'toggledevtools',
 				},
 			],
 		},
-		{
-			role: 'window',
-			submenu: [
-				{
-					role: 'minimize',
-				},
-				{
-					role: 'close',
-				},
-			],
-		},
 	];
-
-	if (process.platform === 'darwin') {
-		template.unshift({
-			label: app.getName(),
-			submenu: [
-				{
-					role: 'about',
-				},
-				{
-					type: 'separator',
-				},
-				{
-					role: 'services',
-					submenu: [],
-				},
-				{
-					type: 'separator',
-				},
-				{
-					role: 'hide',
-				},
-				{
-					role: 'hideothers',
-				},
-				{
-					role: 'unhide',
-				},
-				{
-					type: 'separator',
-				},
-				{
-					role: 'quit',
-				},
-			],
-		});
-		// Edit menu.
-		template[1].submenu.push(
-			{
-				type: 'separator',
-			},
-			{
-				label: 'Speech',
-				submenu: [
-					{
-						role: 'startspeaking',
-					},
-					{
-						role: 'stopspeaking',
-					},
-				],
-			},
-		);
-		// Window menu.
-		template[3].submenu = [
-			{
-				label: 'Close',
-				accelerator: 'CmdOrCtrl+W',
-				role: 'close',
-			},
-			{
-				label: 'Minimize',
-				accelerator: 'CmdOrCtrl+M',
-				role: 'minimize',
-			},
-			{
-				label: 'Zoom',
-				role: 'zoom',
-			},
-			{
-				type: 'separator',
-			},
-			{
-				label: 'Bring All to Front',
-				role: 'front',
-			},
-		];
-	}
-	return template;
 };
